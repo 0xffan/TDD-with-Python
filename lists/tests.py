fe3fadac241c4e76bb91c1602cb07073d4c8ed02
Warren Fan <fweiwei.026@gmail.com>
2017-12-10 11:38:29 +0800

    Adjust model so that items are associated with different list

--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 import re
 
@@ -28,16 +28,24 @@ class HomePageTest(TestCase):
 
         self.assertEqual(self.remove_csrf_token(response.content.decode()), expected_html)
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (even) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_tiems = Item.objects.all()
         self.assertEqual(saved_tiems.count(), 2)
@@ -45,7 +53,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_tiems[0]
         second_saved_item = saved_tiems[1]
         self.assertEqual(first_saved_item.text, 'The first (even) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
 
@@ -54,8 +64,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
